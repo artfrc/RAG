153c9cbe45664a5ed12b7918775d78c067815dc6
Arthur Fernandes Ribeiro Costa <crfernandesarthur@gmail.com>
2026-02-12 11:12:29 -0300

    refactor and changed doc

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from ollama import Client
+from pathlib import Path
 import json
 
-DATA_PATH = "/home/costaarthur/me/projetos/urano/RAG/my-rag/docs/Document 5.pdf"
-PERSIST_DIRECTORY = "/home/costaarthur/me/projetos/urano/RAG/my-rag/chroma_db"
+BASE_DIR = Path(__file__).resolve().parent
+
+DATA_PATH = BASE_DIR / "docs" / "Documentacao.pdf"
+PERSIST_DIRECTORY = BASE_DIR / "chroma_db"
 EMBEDDINGS = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
 COLLECTION_NAME = "urano_doc"
 
 def load_and_split_doc():
-   loader = PyPDFLoader(DATA_PATH)
+   loader = PyPDFLoader(str(DATA_PATH))
    docs = loader.load()
    if docs:
-    print(f"Loaded {len(docs)} pages.") # number of pages
+    print(f"Loaded {len(docs)} pages.\n") # number of pages
     print("Content of the first page:")
-    print(docs[0].page_content) # Print all content of first page
-    print(f"Metadata of the first page: {docs[0].metadata}")
+    print(f"{docs[0].page_content}\n") # Print all content of first page
+    print(f"Metadata of the first page: {docs[0].metadata}\n")
    else:
       print("Failed to load documents.")
       
@@ -28,40 +31,39 @@ def load_and_split_doc():
 
 def embedding_function(all_splits):
    vector_store = Chroma(embedding_function= EMBEDDINGS,
-                         persist_directory = PERSIST_DIRECTORY,
+                         persist_directory = str(PERSIST_DIRECTORY),
                          collection_name = COLLECTION_NAME)
    _ = vector_store.add_documents(documents = all_splits)
    
    return vector_store
    
 def process_user_input(vector_store):
-         question = "My question"
+         question = "Qual o CPX da doc?"
          retrieved_docs = vector_store.similarity_search(question, k=2)
          
-         return retrieved_docs
+         return retrieved_docs, question
          
 
 def main():
    
-   # loading and splting the text in chunka
+   # loading and splting the text in chunks
    splits = load_and_split_doc()
    
-   # creating vectors to store data using Chroma,  AI-native open-source
+   # creating vectors to store data using Chroma, AI-native open-source
    vector_store = embedding_function(splits)
      
    # to process user input
-   retrieved_docs = process_user_input(vector_store)
+   retrieved_docs, question = process_user_input(vector_store)
    
    # generating response
    docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)
    promptJson = {
-      "question": ""
+      "question": question,
+      "context": docs_content
    }
-   
-   
-   
-   
-   
+   prompt = json.dumps(promptJson, ensure_ascii = False)
+   print(f">>> PROMPT, {prompt}")
+
 if __name__ == "__main__":
    main()
    
